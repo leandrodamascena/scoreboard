2de5b1465436012f42fd73dbe135fb1890e49a82
Leandro Damascena <leandro.damascena@gmail.com>
2023-05-27 00:53:06 +0100

    feat(scoreboard): initial commit

---
+++ frontend/script.js
@@ -0,0 +1,108 @@
+// JavaScript function to change the active tab and show the corresponding content
+function changeTab(tabIndex) {
+    // Get all tab buttons and contents
+    var tabButtons = document.getElementsByClassName("tab");
+    var tabContents = document.getElementsByClassName("tab-content");
+  
+    // Remove "active" class from all tab buttons and contents
+    for (var i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove("active");
+      tabContents[i].classList.remove("active");
+    }
+  
+    // Add "active" class to the selected tab button and content
+    console.log(tabIndex)
+    if (tabIndex != undefined) {
+        tabButtons[tabIndex].classList.add("active");
+        tabContents[tabIndex].classList.add("active");
+    }
+    // Perform AJAX call to update the table data
+    updateTableData(tabIndex);
+  }
+  
+  // Function to update table data using AJAX
+  function updateTableData(tabIndex) {
+    var tableId = "tab" + tabIndex + "-table";
+    var table = document.getElementById(tableId);
+  
+    // Simulating AJAX call to retrieve updated data
+    // Replace this with your actual AJAX call to fetch data from the server
+    $.ajax({
+      url: "your-api-endpoint",
+      type: "GET",
+      success: function(data) {
+        // Update the table content
+        var tableBody = table.getElementsByTagName("tbody")[0];
+        tableBody.innerHTML = "";
+  
+        for (var i = 0; i < data.length; i++) {
+          var row = "<tr>" +
+                    "<td>" + data[i].name + "</td>" +
+                    "<td>" + data[i].points + "</td>" +
+                    "<td>" + data[i].country + "</td>" +
+                    "</tr>";
+          tableBody.innerHTML += row;
+        }
+      },
+      error: function() {
+        console.log("Error occurred during AJAX call.");
+      }
+    });
+  }
+  
+  // Initial call to update the default active tab
+  changeTab(0);
+  
+  // Set interval to update the tables every 5 seconds (5000 milliseconds)
+  setInterval(function() {
+    // Get the active tab index
+    var activeTab = document.getElementsByClassName("tab active")[0];
+    var activeTabIndex = Array.from(activeTab.parentNode.children).indexOf(activeTab);
+  
+    // Update the table data for the active tab
+    updateTableData(activeTabIndex);
+  }, 5000);
+  
+
+  // Sort table data based on column index and ascending/descending order
+function sortTable(tabIndex, columnIndex) {
+    var tableId = "tab" + tabIndex + "-table";
+    var table = document.getElementById(tableId);
+    var tableBody = table.getElementsByTagName("tbody")[0];
+    var rows = Array.from(tableBody.getElementsByTagName("tr"));
+  
+    // Sort the rows based on the column values
+    rows.sort(function(a, b) {
+      var valueA = a.getElementsByTagName("td")[columnIndex].innerText;
+      var valueB = b.getElementsByTagName("td")[columnIndex].innerText;
+  
+      if (columnIndex === 1) {
+        valueA = parseInt(valueA);
+        valueB = parseInt(valueB);
+      }
+  
+      if (valueA < valueB) {
+        return -1;
+      } else if (valueA > valueB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  
+    // Reverse the order if the table is already sorted in ascending order
+    // if (table.classList.contains("sorted-asc")) {
+    //   rows.reverse();
+    //   table.classList.remove("sorted-asc");
+    //   table.classList.add("sorted-desc");
+    // } else {
+    //   table.classList.remove("sorted-desc");
+    //   table.classList.add("sorted-asc");
+    // }
+  
+    // Re-append the sorted rows to the table
+    tableBody.innerHTML = "";
+    for (var i = 0; i < rows.length; i++) {
+      tableBody.appendChild(rows[i]);
+    }
+  }
---
+++ scoreboard/producer.py
@@ -0,0 +1,61 @@
+import json
+import random
+from typing import Any, Dict, List
+
+import boto3
+from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from data.players import MOCK_DATA
+
+logger = Logger()
+metrics = Metrics()
+tracer = Tracer()
+
+kinesis_client = boto3.client("kinesis")
+
+
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+@metrics.log_metrics
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    try:
+        # iterating players
+        players: List = []
+        for data in MOCK_DATA:
+            # generating a random score between 0 and 5
+            score: int = random.randint(0, 5)
+            data["score"] = score
+
+            # creating the kinesis record to add using put_records method
+            kinesis_record: Dict[str, Any] = {
+                "PartitionKey": f"shardId-00000000000{score}",
+                "Data": json.dumps(data),
+            }
+
+            # appending records
+            players.append(kinesis_record)
+
+        # sending data to kinesis
+        logger.info("Adding records to Kinesis")
+        response = kinesis_client.put_records(
+            Records=players, StreamName="kds_scoreboard"
+        )
+
+        # put_records can fail some records
+        # creating metric with the number of successes and failed records to insert
+        failed_records = response.get("FailedRecordCount", 0)
+        success_records = len(response.get("Records"))
+
+        metrics.add_metric(
+            "producer_success_records", unit=MetricUnit.Count, value=success_records
+        )
+        metrics.add_metric(
+            "producer_error_records", unit=MetricUnit.Count, value=failed_records
+        )
+
+        return {"message": "success", "FailedRecordCount": failed_records}
+
+    except Exception as exc:
+        logger.exception(f"An error occurred: {exc}")
+        raise
